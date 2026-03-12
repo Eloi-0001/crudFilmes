60d479cbc8eab88fa75f8d78061ba6d436cac715
Eloi-0001 <lucaseloideolibeira@gmail.com>
2026-03-12 02:06:07 -0300

    banco e controller dos testes configurados

--- src/test/java/com/amzingMovies/crudFilmes/controller/filmeControllerTest.java
+++ src/test/java/com/amzingMovies/crudFilmes/controller/filmeControllerTest.java
@@ -1,9 +1,13 @@
 package com.amzingMovies.crudFilmes.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
+@ActiveProfiles("test")
 class filmeControllerTest {
 
     @Test
