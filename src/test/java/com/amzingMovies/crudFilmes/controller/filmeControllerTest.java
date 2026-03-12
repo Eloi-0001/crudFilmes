92a86c0f57a92ee62bd4a256a275f88b018ff003
Eloi-0001 <lucaseloideolibeira@gmail.com>
2026-03-12 01:55:07 -0300

    começando agora com os testes unitarios

---
+++ src/test/java/com/amzingMovies/crudFilmes/controller/filmeControllerTest.java
@@ -0,0 +1,24 @@
+package com.amzingMovies.crudFilmes.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class filmeControllerTest {
+
+    @Test
+    void saveMovie() {
+    }
+
+    @Test
+    void consultMovie() {
+    }
+
+    @Test
+    void deleteMovie() {
+    }
+
+    @Test
+    void updateMovie() {
+    }
+}
