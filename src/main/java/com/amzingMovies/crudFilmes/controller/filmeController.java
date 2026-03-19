68a5e2e54dbc93890f30cc45c9e2639165aada2e
José Lucas Eloi <lucaseloideolibeira@gmail.com>
2026-03-19 20:44:55 -0300

    Add front no projeto Crud ☜(ﾟヮﾟ☜)

--- src/main/java/com/amzingMovies/crudFilmes/controller/filmeController.java
+++ src/main/java/com/amzingMovies/crudFilmes/controller/filmeController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/filmes")
 @CrossOrigin(origins = "*")
-public class filmeController  {
+public class filmeController   {
 
     public FilmesRepository filmesRepository;
 
